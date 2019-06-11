88493d5ecfbdd88a99ede925ec9ae94d843f2ec6
Sandi Metz <sandi@sandimetz.com>
2019-06-11 09:57:08 -0700

    add php

---
+++ bottles/lib/Bottles.php
@@ -0,0 +1,79 @@
+<?php
+
+class Bottles {
+  public function song() {
+    return $this->verses(99, 0);
+  }
+
+  public function verses($starting, $ending) {
+    $verses = [];
+    foreach (range($starting, $ending) as $number) {
+      $verses[] = $this->verse($number);
+    }
+
+    return implode("\n", $verses);
+  }
+
+  public function verse($number) {
+    $bottleNumber     = new BottleNumber($number);
+    $nextBottleNumber = new BottleNumber($bottleNumber->successor());
+
+    return
+      ucfirst($bottleNumber->quantity()) .
+      " {$bottleNumber->container()} of beer on the wall, " .
+      "{$bottleNumber->quantity()} " .
+      "{$bottleNumber->container()} of beer.\n" .
+      "{$bottleNumber->action()}, " .
+      "{$nextBottleNumber->quantity()} " .
+      "{$nextBottleNumber->container()} " .
+      "of beer on the wall.\n";
+  }
+}
+
+class BottleNumber {
+  protected $number;
+
+  public function __construct($number) {
+    $this->number = $number;
+  }
+
+  public function container() {
+    if ($this->number === 1) {
+      return "bottle";
+    } else {
+      return "bottles";
+    }
+  }
+
+  public function quantity() {
+    if ($this->number === 0) {
+      return 'no more';
+    } else {
+      return (string)$this->number;
+    }
+  }
+
+  public function action() {
+    if ($this->number === 0) {
+      return 'Go to the store and buy some more';
+    } else {
+      return "Take {$this->pronoun()} down and pass it around";
+    }
+  }
+
+  public function pronoun() {
+    if ($this->number === 1) {
+      return 'it';
+    } else {
+      return 'one';
+    }
+  }
+
+  public function successor() {
+    if ($this->number === 0) {
+      return 99;
+    } else {
+      return $this->number - 1;
+    }
+  }
+}
