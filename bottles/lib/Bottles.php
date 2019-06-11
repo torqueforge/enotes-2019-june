9eb43933f5210d95f2ce2ec403c7a4e6f875f6a2
Sandi Metz <sandi@sandimetz.com>
2019-06-11 10:01:18 -0700

    add php

---
+++ bottles/lib/Bottles.php
@@ -0,0 +1,97 @@
+<?php
+
+class Bottles {
+  public function song() {
+    return $this->verses(99, 0);
+  }
+
+  public function verses($starting, $ending) {
+    $verses = [];
+    foreach (range($starting, $ending) as $number) {
+      $verses[] = $this->verse($number);
+    }
+
+    return implode("\n", $verses);
+  }
+
+  public function verse($number) {
+    $bottleNumber = BottleNumber::for($number);
+
+    return
+      ucfirst("$bottleNumber of beer on the wall, ") .
+      "{$bottleNumber} of beer.\n" .
+      "{$bottleNumber->action()}, " .
+      "{$bottleNumber->successor()} of beer on the wall.\n";
+  }
+}
+
+class BottleNumber {
+  protected $number;
+
+  public static function for($number) {
+    switch ($number) {
+      case 0:
+        $className = BottleNumber0::class;
+        break;
+      case 1:
+        $className = BottleNumber1::class;
+        break;
+      default:
+        $className = BottleNumber::class;
+        break;
+    }
+    return new $className($number);
+  }
+
+  public function __construct($number) {
+    $this->number = $number;
+  }
+
+  public function __toString() {
+    return $this->quantity() . ' ' . $this->container();
+  }
+
+  public function container() {
+    return "bottles";
+  }
+
+  public function quantity() {
+    return (string)$this->number;
+  }
+
+  public function action() {
+    return "Take {$this->pronoun()} down and pass it around";
+  }
+
+  public function pronoun() {
+    return 'one';
+  }
+
+  public function successor() {
+    return static::for($this->number - 1);
+  }
+}
+
+class BottleNumber0 extends BottleNumber {
+  public function quantity() {
+    return 'no more';
+  }
+
+  public function action() {
+    return 'Go to the store and buy some more';
+  }
+
+  public function successor() {
+    return static::for(99);
+  }
+}
+
+class BottleNumber1 extends BottleNumber {
+  public function container() {
+    return "bottle";
+  }
+
+  public function pronoun() {
+    return 'it';
+  }
+}
