7143c26f59b2fe63362c72625e4379d3202a3f75
Matt Fonda <mfonda@enotes.com>
2019-06-12 20:57:45 +0000

    add factory examples

--- bottles/lib/Bottles.php
+++ bottles/lib/Bottles.php
@@ -1,97 +1,145 @@
 <?php
 
 class Bottles {
-  public function song() {
-    return $this->verses(99, 0);
-  }
-
-  public function verses($starting, $ending) {
-    $verses = [];
-    foreach (range($starting, $ending) as $number) {
-      $verses[] = $this->verse($number);
+    public function song() {
+        return $this->verses(99, 0);
     }
 
-    return implode("\n", $verses);
-  }
+    public function verses($starting, $ending) {
+        $verses = [];
+        foreach (range($starting, $ending) as $number) {
+            $verses[] = $this->verse($number);
+        }
 
-  public function verse($number) {
-    $bottleNumber = $this->bottleNumberFor($number);
-    $nextBottleNumber = $this->bottleNumberFor($bottleNumber->successor());
-
-    return
-      ucfirst($bottleNumber->quantity()) .
-      " {$bottleNumber->container()} of beer on the wall, " .
-      "{$bottleNumber->quantity()} {$bottleNumber->container()} of beer.\n" .
-      "{$bottleNumber->action()}, " .
-      "{$nextBottleNumber->quantity()} " .
-      "{$nextBottleNumber->container()} " .
-      "of beer on the wall.\n";
-  }
-
-  public function bottleNumberFor($number) {
-    switch ($number) {
-      case 0:
-        $className = BottleNumber0::class;
-        break;
-      case 1:
-        $className = BottleNumber1::class;
-        break;
-      default:
-        $className = BottleNumber::class;
-        break;
+        return implode("\n", $verses);
     }
-    return new $className($number);
-  }
+
+    public function verse($number) {
+        $bottleNumber = BottleNumber::for($number);
+
+        return
+            ucfirst($bottleNumber) . " of beer on the wall, " .
+            "{$bottleNumber} of beer.\n" .
+            "{$bottleNumber->action()}, " .
+            "{$bottleNumber->successor()} " .
+            "of beer on the wall.\n";
+    }
 }
 
+
 class BottleNumber {
-  protected $number;
+    protected $number;
 
-  public function __construct($number) {
-    $this->number = $number;
-  }
+    public function __construct($number) {
+        $this->number = $number;
+    }
 
-  public function container() {
-    return "bottles";
-  }
+    public static function for(int $number) {
+        $classes = [
+            'BottleNumber0',
+            'BottleNumber1',
+            'BottleNumber',
+        ];
+        foreach ($classes as $className) {
+            if ($className::handles($number)) {
+                return new $className($number);
+            }
+        }
 
-  public function quantity() {
-    return (string)$this->number;
-  }
 
-  public function action() {
-    return "Take {$this->pronoun()} down and pass it around";
-  }
+        $classNames = [
+            0 => 'BottleNumber0',
+            1 => 'BottleNumber1',
+        ];
+        if (isset($classNames[$number])) {
+            return new $classNames[$number]($number);
+        } else {
+            return new BottleNumber($number);
+        }
 
-  public function pronoun() {
-    return 'one';
-  }
 
-  public function successor() {
-    return $this->number - 1;
-  }
+        $className = 'BottleNumber' . $number;
+        if (class_exists($className)) {
+            return new $className($number);
+        } else {
+            return new BottleNumber($number);
+        }
+
+        switch ($number) {
+        case 0:
+            $className = BottleNumber0::class;
+            break;
+        case 1:
+            $className = BottleNumber1::class;
+            break;
+        default:
+            $className = BottleNumber::class;
+            break;
+        }
+        return new $className($number);
+    }
+
+    public function container() {
+        return "bottles";
+    }
+
+    public function quantity() {
+        return (string)$this->number;
+    }
+
+    public function action() {
+        return "Take {$this->pronoun()} down and pass it around";
+    }
+
+    public function pronoun() {
+        return 'one';
+    }
+
+    public function successor() {
+        return BottleNumber::for($this->number - 1);
+    }
+
+    public function __toString()
+    {
+        return $this->quantity() . ' ' . $this->container();
+    }
+
+    public static function handles($number): bool
+    {
+        return true;
+    }
 }
 
 class BottleNumber0 extends BottleNumber {
-  public function quantity() {
-    return 'no more';
-  }
+    public function quantity() {
+        return 'no more';
+    }
 
-  public function action() {
-    return 'Go to the store and buy some more';
-  }
+    public function action() {
+        return 'Go to the store and buy some more';
+    }
 
-  public function successor() {
-    return 99;
-  }
+    public function successor() {
+        return BottleNumber::for(99);
+    }
+
+    public static function handles($number): bool
+    {
+        return $number == 0;
+    }
 }
 
 class BottleNumber1 extends BottleNumber {
-  public function container() {
-    return "bottle";
-  }
+    public function container() {
+        return "bottle";
+    }
 
-  public function pronoun() {
-    return 'it';
-  }
+    public function pronoun() {
+        return 'it';
+    }
+
+    public static function handles($number): bool
+    {
+        return $number == 1;
+    }
 }
